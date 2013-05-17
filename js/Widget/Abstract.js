4c4204584179696c2dbd8784adcb429058bd17fa
Konstantin Kitmanov <doctor.hogart@gmail.com>
2013-05-18 01:11:16 +0400

    lang support in input fields

--- js/Widget/Abstract.js
+++ js/Widget/Abstract.js
@@ -6,7 +6,21 @@ define(
     function (Chitin, _) {
         var WidgetAbstract = Chitin.Widget.extend({
             render: function (data) {
-                data = _.extend(data, {locale: this.bus.locale});
+                var bus = this.bus,
+                    prefAttrs = bus.prefs.attrs,
+                    pair = prefAttrs.pair;
+
+                data = _.extend(
+                    data,
+                    {
+                        locale: bus.locale,
+                        lang: {
+                            'interface': prefAttrs.interface,
+                            learn: pair ? pair.learn : '',
+                            native: pair ? pair.native : ''
+                        }
+                    }
+                );
 
                 WidgetAbstract.__super__.render.call(this, data);
             }
