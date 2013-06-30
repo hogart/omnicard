686cb61b1504d55bc864e00fbac2294709923baa
Konstantin Kitmanov <doctor.hogart@gmail.com>
2013-07-01 00:49:12 +0400

    support for touch events where appropriate

--- js/Widget/Abstract.js
+++ js/Widget/Abstract.js
@@ -4,27 +4,52 @@ define(
         '_'
     ],
     function (Chitin, _) {
-        var WidgetAbstract = Chitin.Widget.extend({
+        var widgetPrototype = {
             render: function (data) {
                 var bus = this.bus,
                     prefAttrs = bus.prefs.attrs,
-                    pair = prefAttrs.pair;
+                    pair = prefAttrs.pair,
 
-                data = _.extend(
-                    data,
-                    {
+                    commons = {
                         locale: bus.locale,
                         lang: {
                             'interface': prefAttrs.interface,
                             learn: pair ? pair.learn : '',
                             native: pair ? pair.native : ''
                         }
-                    }
+                    };
+
+                data = _.extend(
+                    data,
+                    commons
                 );
 
                 WidgetAbstract.__super__.render.call(this, data);
             }
-        });
+        };
+
+        if ('ontouchend' in document) {
+            widgetPrototype.delegateEvents = function (events) {
+                if (!(events || (events = _.result(this, 'events')))) return;
+
+                var touchEvents = {};
+                _.each(events, function (value, key) {
+                    var k = key;
+                    if (key.substr(0, 6) == 'click ') {
+                        k = key.replace(/^click /, 'touchend ');
+                    }
+
+                    touchEvents[k] = value
+                });
+
+                this._delegateEvents(touchEvents);
+            }
+        }
+
+        /**
+         * @class
+         */
+        var WidgetAbstract = Chitin.Widget.extend(widgetPrototype);
 
         return WidgetAbstract;
     }
